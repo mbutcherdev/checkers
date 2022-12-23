fa1399fa89e5fc02847071a721dc71cd719ab300
MattButcherUK <mbutcher093@gmail.com>
2022-12-23 12:14:32 +0000

    A working version of the Checkers game

---
+++ venv/checkers/constants.py
@@ -0,0 +1,26 @@
+# File contains all 'set' veriables, such as the window size, the window title, etc.
+# Change these values to change the game
+
+# Import modules
+import pygame
+
+# --- Set up the pygame display window ---
+WIDTH = 800
+HEIGHT = 800
+TITLE = "Checkers"
+FPS = 60
+# --- Set up the game board itself ---
+ROWS = 8
+COLS = 8
+SQUARE_SIZE = WIDTH // COLS
+# --- Set up the colours ---
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKWOOD = (85, 52, 43)
+WOOD = (210, 171, 111)  # Yellow-ish brown
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown_image.png'), (44, 25))
+
---
+++ venv/checkers/game.py
@@ -0,0 +1,72 @@
+import pygame
+from .constants import WHITE, BLACK, GREEN, SQUARE_SIZE
+from .board import Board
+
+
+class Game:
+    def __init__(self, window):
+        self._init()
+        self.window = window
+
+    def update(self):
+        self.board.draw(self.window)
+        self.draw_valid_moves(self.valid_moves)
+        pygame.display.update()
+
+    def _init(self):
+        self.selected = None
+        self.board = Board()
+        self.turn = BLACK
+        self.player = "Black"
+        self.valid_moves = {}
+
+    def reset(self):
+        self._init()
+
+    def winner(self):
+        return self.board.winner()
+
+    def select(self, row, col):
+        if self.selected:
+            result = self._move(row, col)
+            if not result:
+                self.selected = None
+                self.select(row, col)
+
+        piece = self.board.get_piece(row, col)
+        if piece != 0 and piece.color == self.turn:
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
+
+        return False
+
+    def _move(self, row, col):
+        piece = self.board.get_piece(row, col)
+        if self.selected and piece == 0 and (row, col) in self.valid_moves:
+            self.board.move(self.selected, row, col)  # Move currently selected piece to the new position
+            print(f"Moved {self.player} to {row}, {col}")
+            skipped = self.valid_moves[(row, col)]
+            if skipped:
+                self.board.remove(skipped)
+            self.change_turn()
+            print(f"{self.player}'s move!")
+        else:
+            return False
+
+        return True
+
+    def draw_valid_moves(self, moves):
+        for move in moves:
+            row, col = move
+            pygame.draw.circle(self.window, GREEN,
+                               (col * SQUARE_SIZE + SQUARE_SIZE // 2, row * SQUARE_SIZE + SQUARE_SIZE // 2), 5)
+
+    def change_turn(self):
+        self.valid_moves = {}
+        if self.turn == BLACK:
+            self.turn = WHITE
+            self.player = "White"
+        else:
+            self.turn = BLACK
+            self.player = "Black"
